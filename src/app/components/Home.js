898df48e47352e1ccd61cd1809f78ce953583fe7
Hadi Yousufi <reach.hadiy@gmail.com>
2018-03-28 16:09:33 -0500

    adds user object as a prop to home comp

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,11 +1,21 @@
 import React from "react";
+import PropTypes from 'prop-types';
+
 
 export class Home extends React.Component {
   render() {
+
     return (
       <div>
-        <p>In a new Component!</p>
+        <p>Name: {this.props.user.name}</p>
+        <h4>User's Hobbies</h4>
+        <ul>{this.props.user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>)}</ul>
       </div>
     );
   }
+}
+
+Home.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number,
 }
--- src/app/index.js
+++ src/app/index.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { Header } from './components/Header';
-import { Home } from './components/Home';
+import React from "react";
+import { render } from "react-dom";
+import { Header } from "./components/Header";
+import { Home } from "./components/Home";
 
 class App extends React.Component {
-
     render() {
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-xs-10 col-xs-offset-1">
-                        <Header />
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-xs-10 col-xs-offset-1">
-                        <Home />
-                    </div>
-                </div>
-            </div>
-        );
-    }
+        let user = {
+            name: "Alex",
+            hobbies: ["coding", "travel", "tech gadgets"]
+        }
+
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-10 col-xs-offset-1">
+            <Header />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-10 col-xs-offset-1">
+            <Home name={"Hadi"} age={27} user={user} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-render(<App />, window.document.getElementById('app')); 
+render(<App />, window.document.getElementById("app"));
