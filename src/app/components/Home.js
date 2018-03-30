16922898ec98e9b4344f6371f039d9e94f7020d4
Hadi Yousufi <reach.hadiy@gmail.com>
2018-03-30 13:59:21 -0500

    adds oClick event listener to button

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,16 +1,24 @@
 import React from "react";
-import PropTypes from 'prop-types';
-
+import PropTypes from "prop-types";
 
 export class Home extends React.Component {
-  render() {
+  constructor(props) {
+    super();
+    this.age = props.age;
+  }
 
+  onMakeOlder() {
+    this.age += 3;
+    console.log(this.age);
+  }
+
+  render() {
     return (
       <div>
-      <p>This is the Home Component</p>  
+        <p>This is the Home Component</p>
         <p>Name: {this.props.name}</p>
-        <p>Age: {this.props.age}</p>
-      <button className="btn btn-primary">Make me older!</button>
+        <p>Age: {this.age}</p>
+        <button onClick={() => this.onMakeOlder()} className="btn btn-primary">Make me older!</button>
       </div>
     );
   }
@@ -18,5 +26,5 @@ export class Home extends React.Component {
 
 Home.propTypes = {
   name: PropTypes.string,
-  age: PropTypes.number,
-}
+  age: PropTypes.number
+};
